906d61b56bfd58872b286e3330f8470998a87037
codyhertz <codyhertz@yahoo.com>
2021-01-28 10:09:14 -0600

    added atm services

--- src/dollars_bank_atm.js
+++ src/dollars_bank_atm.js
@@ -8,8 +8,10 @@ export class ATM
     this.currentUser = null;
   }
 
-  addUser(cust)
+  addUser(name, address, number, userId, password, initialDeposit)
   {
+    let cust = new Customer(name, address, number, userId, password, initialDeposit);
+    this.currentUser = cust;
     this.users.push(cust);
   }
 
@@ -45,5 +47,61 @@ export class ATM
   setCurrentUser(currentUser)
   {
     this.currentUser = currentUser;
+  }
+
+  checkUserBalance()
+  {
+    if(this.currentUser != null)
+    {
+      this.currentUser.checkBalance();
+    }
+  }
+
+  checkUserTransactions()
+  {
+    if(this.currentUser != null)
+    {
+      this.currentUser.getTransactions();
+    }
+  }
+
+  depositUserChecking(amount)
+  {
+    if(this.currentUser != null)
+    {
+      this.currentUser.depositChecking(amount);
+    }
+  }
+
+  depositUserSavings(amount)
+  {
+    if(this.currentUser != null)
+    {
+      this.currentUser.depositSavings(amount);
+    }
+  }
+
+  withdrawlUserChecking(amount)
+  {
+    if(this.currentUser != null)
+    {
+      this.currentUser.withdrawlUserChecking(amount);
+    }
+  }
+
+  withdrawlUserSavings(amount)
+  {
+    if(this.currentUser != null)
+    {
+      this.currentUser.withdrawlUserSavings(amount);
+    }
+  }
+
+  userTransfer(checkingToSavings, amount)
+  {
+    if(this.currentUser != null)
+    {
+      this.currentUser.transfer(checkingToSavings, amount);
+    }
   }
 }
